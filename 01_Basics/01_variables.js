b77fdc820f7d16c229fef9d6c643fe6e879cf0e3
moohiit <pmohit645@gmail.com>
2023-09-11 23:20:03 +0530

    New files added

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,9 +1,22 @@
-const accountId = 1444112
-let accountEmail = "mohit@gmail.com"
-var accountPassword = "12546"
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
 accountCity = "Jaipur"
+let accountState;
 
-//accountId = 2 // not allowed
+// accountId = 2 // not allowed
 
-console.log(accountId)
 
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
