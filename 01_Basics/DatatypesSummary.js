136668af1ba055619dd779932679642738d91f75
moohiit <109367447+moohiit@users.noreply.github.com>
2023-09-12 17:39:10 +0000

    file udated

--- 01_Basics/DatatypesSummary.js
+++ 01_Basics/DatatypesSummary.js
@@ -16,29 +16,45 @@ const anotherId = Symbol('123') //symbol
 // console.log(Id==anotherId);
 
 const bigInteger = 322323232323455455555555232n //BigInt
-console.log(typeof bigInteger)
+// console.log(typeof bigInteger)
 
 
 //Reference (Non Primitive)
 //Array, Objects, Functions
 const heros = ["Akshay","Shahrukh", "Salman"] //Array
-console.log(typeof heros)
+// console.log(typeof heros)
 
 const myObj = {  //object
     name:"Mohit",
     agw: 22,
 }
-console.log(typeof myObj)
+// console.log(typeof myObj)
 
 
 const myFunction = function(){   //function
     console.log("Hello World");
 }
-console.log(typeof myFunction)
-
+// console.log(typeof myFunction)
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //Stack(Primitive) Heap (Non Primitive)
 
+let myyoutubename = "hiteshchaudharydotcom"
 
+let anothername = myyoutubename
+anothername = "chaiandcode"
+
+console.log(myyoutubename)
+console.log(anothername);
+
+let userOne ={
+    email:"user@gmail.com",
+    upi:"user1@ybl"
+}
+
+let userTwo = userOne //reference to userOne is used.
+userTwo.email="user2@gmail.com" //it will also update the userOne email
+
+console.log(userOne.email)
+console.log(userTwo.email)
